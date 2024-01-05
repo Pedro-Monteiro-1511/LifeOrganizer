9345457cae7e6c5f9a1d3cf39130b2c0a562f687
PedroMonteiro_1511 <pedro.monteiro1511@outlook.pt>
2024-01-05 23:42:48 +0000

    First Styles added

---
+++ LifeOrganizer/components/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import { View, Text, Image } from 'react-native';
+import Constants from 'expo-constants';
+
+const StatusbarHeight = Constants.statusBarHeight;
+
+// colors
+export const colors = {
+    primary: "#ffffff",
+    secondary: "#E5E7EB",
+    third: "#1F2937",
+    darkLight: "#9CA3AF",
+    brand: "#6D28D9",
+    green: "#10B981",
+    red: "#EF4444",
+}
+
+const { primary, secondary, third, darkLight, brand, green, red } = colors;
+
+export const StyledContainer = styled.View`
+    flex: 1;
+    padding: 25px;
+    paddding-tio: ${StatusbarHeight +10}px;
+    background-color: ${primary};
+`
+
+export const InnerContainer = styled.View`
+    flex: 1;
+    width: 100%;
+    align-items: center;
+`;
+
+export const PageTitle = styled.Text`
+    font-size: 30px;
+    text-align: center;
+    font-weight: bold;
+    color: ${brand}
+    padding: 10px;
+`;
